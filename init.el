e9b2aca2e3d5fd437da11e201f49ff0240d20520
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2016-06-06 18:16:57 +0200

    reorders search tools

--- init.el
+++ init.el
@@ -204,7 +204,7 @@ values."
    ;; List of search tool executable names. Spacemacs uses the first installed
    ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
    ;; (default '("ag" "pt" "ack" "grep"))
-   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   dotspacemacs-search-tools '("pt" "ag" "ack" "grep")
    ;; The default package repository used if no explicit repository has been
    ;; specified with an installed package.
    ;; Not used for now. (default nil)
@@ -221,6 +221,11 @@ user code."
   "Configuration function for user code.
  This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
+
+  ;; Start daemon
+  ;; (unless (server-running-p)
+  ;;         (server-start))
+
   ;; Make linums relative by default
   (global-linum-mode nil)
   (linum-relative-toggle)
