b77480b5971eb266d47bb8b31b33e326c4795ad2
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2016-07-18 17:02:13 +0200

    Adds multiple cursors

--- init.el
+++ init.el
@@ -58,7 +58,10 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages
+   '(
+     ;; multiple-cursors
+     )
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
@@ -225,6 +228,9 @@ layers configuration. You are free to put any user code."
   ;; Start daemon
   ;; (unless (server-running-p)
   ;;         (server-start))
+
+  ;; (require 'multiple-cursors)
+  (global-evil-mc-mode 1)
 
   ;; Make linums relative by default
   (global-linum-mode nil)
